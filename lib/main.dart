02652fb85569251bd00502b2c47746b7a5c420ff
Salih <salihyildirimce@gmail.com>
2023-10-12 23:59:32 +0300

    brr

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( // borrowlist icin delete eklenecek.
         title: 'Flutter Demo',
         theme: ThemeData(
           // This is the theme of your application.
